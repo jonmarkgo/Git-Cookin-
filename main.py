f5dcefbe24aeb1fff443c4d2a5b6edb9950cddae
ChrisSmith <collegelabs@gmail.com>
2011-11-12 12:50:40 -0500

    sample handler

--- main.py
+++ main.py
@@ -17,15 +17,21 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
+import os
+from google.appengine.ext.webapp import template
+from recipes import RecipesHandler
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+		template_values = { }
+		path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+		self.response.out.write(template.render(path, template_values))
 
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
+    application = webapp.WSGIApplication([('/', MainHandler),
+										 ('/recipes', RecipesHandler)
+										],debug=True)
     util.run_wsgi_app(application)
 
 
