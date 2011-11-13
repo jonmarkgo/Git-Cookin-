174221dc574d2ca5a634e4c9f330a2edfab26322
ChrisSmith <collegelabs@gmail.com>
2011-11-13 12:21:01 -0500

    fixed broken model

--- app/models/step_ingredient.rb
+++ app/models/step_ingredient.rb
@@ -3,5 +3,5 @@ class StepIngredient < ActiveRecord::Base
     belongs_to :step
     belongs_to :ingredient
     
-    accepts_nested_attributes_for :steps, :reject_if => lambda {|a| a[:instruction].blank? }, :allow_destroy => true
+    accepts_nested_attributes_for :step, :reject_if => lambda {|a| a[:instruction].blank? }, :allow_destroy => true
 end
