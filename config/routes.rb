b84ba0b48577fc5a432d7ca4913d68c82cd012cb
ChrisSmith <collegelabs@gmail.com>
2011-11-12 20:09:27 -0500

    removing log

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Gitcookin::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
  
-  root :to => "ingredients#index"
+  root :to => "recipes#index"
 
   # See how all your routes lay out with "rake routes"
 
